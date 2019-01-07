e2aaa87b8c6e8de41bd106f6538bcae7641835bd
edayldz <43698343+edayldz@users.noreply.github.com>
2019-01-08 02:35:53 +0300

    Update main.r

--- main.r
+++ main.r
@@ -6,7 +6,6 @@ require(data.table)
 require(TunePareto)
 require(glmnet)
 
-setwd('C:/Users/yasemin/Desktop/fall18-instructor-master')
 
 testStart=as.Date('2018-12-31')
 trainStart=as.Date('2012-07-15')
@@ -17,8 +16,8 @@ source('feature_extraction.r')
 source('performance_metrics.r')
 source('train_models.r')
 
-matches_data_path='C:/Users/yasemin/Downloads/df9b1196-e3cf-4cc7-9159-f236fe738215_matches.rds'
-odd_details_data_path='C:/Users/yasemin/Downloads/df9b1196-e3cf-4cc7-9159-f236fe738215_odd_details.rds'
+matches_data_path='df9b1196-e3cf-4cc7-9159-f236fe738215_matches.rds'
+odd_details_data_path='df9b1196-e3cf-4cc7-9159-f236fe738215_odd_details.rds'
 
 
 # read data
@@ -32,16 +31,16 @@ matches=matches_data_preprocessing(matches_raw)
 odd_details=details_data_preprocessing(odd_details_raw,matches)
 
 # extract open and close odd type features from multiple bookmakers
-features=extract_features.openclose(matches,odd_details,pMissThreshold=rem_miss_threshold,trainStart,testStart)
+features=extract_features.CombineFeatures(matches,odd_details,pMissThreshold=rem_miss_threshold,trainStart,testStart)
+
 
 # divide data based on the provided dates 
 train_features=features[Match_Date>=trainStart & Match_Date<testStart] 
 test_features=features[Match_Date>=testStart] 
 
-# run glmnet on train data with tuning lambda parameter based on RPS and return predictions based on lambda with minimum RPS
-predictions=train_glmnet(train_features, test_features,not_included_feature_indices=c(1:5), alpha=1,nlambda=50, tune_lambda=TRUE,nofReplications=5,nFolds=10,trace=T)
-    
 
+# run glmnet on train data with tuning lambda parameter based on RPS and return predictions based on lambda with minimum RPS
+predictions=train_glmnet(train_features, test_features,not_included_feature_indices=c(1,16:19), alpha=1,nlambda=50, tune_lambda=TRUE,nofReplications=5,nFolds=10,trace=T)
 
 MatchDate=matches[,c('matchId','Match_Date','Home','Away'),with=F]
 
