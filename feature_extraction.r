09b49ba84907727d62523d74ae142bba343232a2
Unknown <yasemin.gokdag@boun.edu.tr>
2019-01-08 05:05:11 +0300

    feature extract update

--- feature_extraction.r
+++ feature_extraction.r
@@ -84,8 +84,8 @@ extract_features.PreviousResults<- function(matches){
   PreviousMatches[order(Match_Date),TwoWay4:=shift(Winner,4,type='lag'),by=TeamsId ]
   PreviousMatches[order(Match_Date),TwoWay5:=shift(Winner,5,type='lag'),by=TeamsId ]
   
-  PreviousMatches$Identical5Freq=apply(PreviousMatches[,c('Identical1','Identical2','Identical3','Identical4','Identical5'),with=F],1,getmode)
-  PreviousMatches$TwoWay5Freq=apply(PreviousMatches[,c('TwoWay1','TwoWay2','TwoWay3','TwoWay4','TwoWay5'),with=F],1,getmode)
+  # PreviousMatches$Identical5Freq=apply(PreviousMatches[,c('Identical1','Identical2','Identical3','Identical4','Identical5'),with=F],1,getmode)
+  # PreviousMatches$TwoWay5Freq=apply(PreviousMatches[,c('TwoWay1','TwoWay2','TwoWay3','TwoWay4','TwoWay5'),with=F],1,getmode)
   
   PreviousMatches$Identical2Freq=apply(PreviousMatches[,c('Identical1','Identical2'),with=F],1,getmode)
   PreviousMatches$TwoWay2Freq=apply(PreviousMatches[,c('TwoWay1','TwoWay2'),with=F],1,getmode)
@@ -94,26 +94,28 @@ extract_features.PreviousResults<- function(matches){
   PreviousMatches$TwoWay1Freq=apply(PreviousMatches[,c('TwoWay1'),with=F],1,getmode)
   
   
-  PreviousMatches=PreviousMatches[,c('matchId','Identical5Freq','TwoWay5Freq','Identical2Freq','TwoWay2Freq','Identical1Freq','TwoWay1Freq'),with=F]
+  PreviousMatches=PreviousMatches[,c('matchId','Identical2Freq','TwoWay2Freq','Identical1Freq','TwoWay1Freq'),with=F]
   return(PreviousMatches)
   
 }
 
-
 extract_features.HomeAwayPerformances<- function(matches,nmatch = 5){
-data=matches
+
+data= matches
 #Get mode(result) of the previous 10 games per home per game 
 data <- data[order(Home, -Match_Date)][, HomeMatchRank := 1:.N, list(Home)]
 Bound <- max(data$HomeMatchRank)
-
 
 HomeResults <- data.table()
 for (i in (1:Bound))
 {
   matchIds=data[HomeMatchRank==i]$matchId
   select_data <- data[HomeMatchRank > i & HomeMatchRank <= i+nmatch][, list(lapply(Match_Result,getmode)), by = c("Home")]
+  if (nrow(select_data) >0) {
+  colnames=colnames(select_data)
+  select_data <-as.data.table(matrix(unlist(select_data), nrow=nrow(select_data)),stringsAsFactors=FALSE)
+  setnames(select_data,colnames)
   select_data <- select_data[, (lapply(.SD,getmode)), by = c("Home")]
- if (nrow(select_data) >0) {
   select_data=cbind(select_data,matchIds)
   HomeResults <- rbind(HomeResults,select_data)
  }
@@ -121,30 +123,32 @@ for (i in (1:Bound))
 
 setnames(HomeResults,'V1','HomeResult')
 
-
 data <- data[order(Away, -Match_Date)][, AwayMatchRank := 1:.N, list(Away)]
 Bound <- max(data$AwayMatchRank)
-
 
 AwayResults <- data.table()
 for (i in (1:Bound))
 {
+  
   matchIds=data[AwayMatchRank==i]$matchId
   select_data <- data[AwayMatchRank > i & AwayMatchRank <= i+nmatch][, list(lapply(Match_Result,getmode)), by = c("Away")]
-  select_data <- select_data[, (lapply(.SD,getmode)), by = c("Away")]
   if (nrow(select_data) >0) {
-    select_data=cbind(select_data,matchIds)
-    AwayResults <- rbind(AwayResults,select_data)
+  colnames=colnames(select_data)
+  select_data <-as.data.table(matrix(unlist(select_data), nrow=nrow(select_data)),stringsAsFactors=FALSE)
+  setnames(select_data,colnames)
+  select_data <- select_data[, (lapply(.SD,getmode)), by = c("Away")]
+  select_data=cbind(select_data,matchIds)
+  AwayResults <- rbind(AwayResults,select_data)
   }
 }
 
 setnames(AwayResults,'V1','AwayResult')
 
-
 setkey(AwayResults,matchIds)
 setkey(HomeResults,matchIds)
 
 Results=AwayResults[HomeResults]
+
 return(Results)
 }
 
@@ -154,6 +158,50 @@ extract_features.ELORatings <- function(){
   # read data
   ELORatings=fread("Elo_Ratings.txt")
   return(ELORatings)
+}
+
+extract_features.GoalDifferences<- function(matches){
+  
+  Teams =as.data.table(unique(matches$Home)) 
+  Teams2 =as.data.table(unique(matches$Away) )
+  Teams=unique(rbind(Teams,Teams2))
+  
+  
+  Goal_v=data.table()
+  Goal_va=data.table()
+  
+  for (i in 1:nrow(Teams)) 
+  {
+    Team = Teams[i]
+    PreviousMatches=matches[Home==Team$V1 | Away==Team$V1][order(Match_Date)]
+    PreviousMatches[Home==Team$V1,GolFor := Home_Score]
+    PreviousMatches[Away==Team$V1,GolFor := Away_Score]
+    PreviousMatches[Away==Team$V1,GolAgainst := Home_Score]
+    PreviousMatches[Home==Team$V1,GolAgainst := Away_Score]
+    
+    PreviousMatches[order(Match_Date),GoalDif:=( shift(GolFor,1,type='lag')-shift(GolAgainst,1,type='lag')+
+                                                   shift(GolFor,2,type='lag')-shift(GolAgainst,2,type='lag')+
+                                                   shift(GolFor,3,type='lag')-shift(GolAgainst,3,type='lag')+                                    +
+                                                   shift(GolFor,4,type='lag')-shift(GolAgainst,4,type='lag')+
+                                                   shift(GolFor,5,type='lag')-shift(GolAgainst,5,type='lag')+
+                                                   shift(GolFor,6,type='lag')-shift(GolAgainst,6,type='lag')) ]
+    
+    Goal=PreviousMatches[Home==Team$V1,c('matchId','GoalDif'),with=F]
+    GoalA=PreviousMatches[Away==Team$V1,c('matchId','GoalDif'),with=F]
+    setnames(Goal,'GoalDif','HomeGoalDif')
+    setnames(GoalA,'GoalDif','AwayGoalDif')
+    Goal_v=rbind(Goal_v,Goal)
+    Goal_va=rbind(Goal_va,GoalA)
+  }
+  
+  setkey(Goal_v,matchId)
+  setkey(Goal_va,matchId)
+  GoalDifferences=Goal_va[Goal_v]
+  GoalDifferences[,DifferenceinGoalDif:=HomeGoalDif-AwayGoalDif]
+  GoalDifferences=GoalDifferences[,c(1,4),with=F]
+  return(GoalDifferences)
+  
+  
 }
 
 
@@ -195,7 +243,7 @@ extract_features.FIFARatings <- function(matches){
 
 
 extract_features.CombineFeatures <- function(matches,odd_details,pMissThreshold=0.01,trainStart,testStart){
- 
+  
   OddFeatures=extract_features.openclose(matches,odd_details,pMissThreshold=0.01,trainStart,testStart)
   
   setkey(OddFeatures,matchId)
@@ -237,46 +285,107 @@ extract_features.CombineFeatures <- function(matches,odd_details,pMissThreshold=
   setkey(PreviousResults,matchId)
   Data=PreviousResults[Data]
   
-  Data[Identical5Freq==Away,Identical5Freq:=2]
-  Data[Identical5Freq==Home,Identical5Freq:=1]
-  Data[Identical5Freq=='Tie',Identical5Freq:=0]
-  Data[TwoWay5Freq==Away,TwoWay5Freq:=2]
-  Data[TwoWay5Freq==Home,TwoWay5Freq:=1]
-  Data[TwoWay5Freq=='Tie',TwoWay5Freq:=0]
+  # Data[Identical5Freq==Away,Identical5Freq:=2]
+  # Data[Identical5Freq==Home,Identical5Freq:=1]
+  # Data[Identical5Freq=='Tie',Identical5Freq:=0]
+  # Data[TwoWay5Freq==Away,TwoWay5Freq:=2]
+  # Data[TwoWay5Freq==Home,TwoWay5Freq:=1]
+  # Data[TwoWay5Freq=='Tie',TwoWay5Freq:=0]
   Data[Identical2Freq==Away,Identical2Freq:=2]
   Data[Identical2Freq==Home,Identical2Freq:=1]
   Data[Identical2Freq=='Tie',Identical2Freq:=0]
+  
+  Data$Identical2Freq <- as.numeric(Data$Identical2Freq)
+  
   Data[TwoWay2Freq==Away,TwoWay2Freq:=2]
   Data[TwoWay2Freq==Home,TwoWay2Freq:=1] 
   Data[TwoWay2Freq=='Tie',TwoWay2Freq:=0]
+  
+  Data$TwoWay2Freq <- as.numeric(Data$TwoWay2Freq)
+  
+  
   Data[Identical1Freq==Away,Identical1Freq:=2]
   Data[Identical1Freq==Home,Identical1Freq:=1]
   Data[Identical1Freq=='Tie',Identical1Freq:=0]
+  
+  Data$Identical1Freq <- as.numeric(Data$Identical1Freq)
+  
   Data[TwoWay1Freq==Away,TwoWay1Freq:=2]
   Data[TwoWay1Freq==Home,TwoWay1Freq:=1]
   Data[TwoWay1Freq=='Tie',TwoWay1Freq:=0]
 
+  Data$TwoWay1Freq <- as.numeric(Data$TwoWay1Freq)
   
   Data[AwayResult=='Home',AwayResult:=1]
   Data[AwayResult=='Away',AwayResult:=2]
   Data[AwayResult=='Tie',AwayResult:=0]
   
+  Data$AwayResult <- as.numeric(Data$AwayResult)
+  
   Data[HomeResult=='Home',HomeResult:=1]
   Data[HomeResult=='Away',HomeResult:=2]
   Data[HomeResult=='Tie',HomeResult:=0]
   
+  Data$HomeResult <- as.numeric(Data$HomeResult)
+  
   Data[Identical1Freq=='Tie',Identical1Freq:=0]
   
+  Data$Identical1Freq <- as.numeric(Data$Identical1Freq)
+   
   setkey(Data,matchId)
   setkey(OddFeatures,matchId)
   Data=Data[OddFeatures]
   Data$Away=NULL
   Data$Home=NULL
+  
+  GoalDiff=  extract_features.GoalDifferences(matches)
+  setkey(GoalDiff,matchId)
+  setkey(Data,matchId)
+  Data=GoalDiff [Data]
+  
+  setcolorder(Data, c("leagueId","Match_Date","Match_Hour","Match_Result","matchId","DifferenceinGoalDif","Identical2Freq","TwoWay2Freq","Identical1Freq","TwoWay1Freq","AwayResult","HomeResult","AttackDiff","MidfieldDiff","DefenseDiff","OverallDiff","AwayELO","HomeELO","Odd_Open_odd1_10Bet","Odd_Open_odd1_12BET","Odd_Open_odd1_188BET","Odd_Open_odd1_BetVictor","Odd_Open_odd1_Betclic","Odd_Open_odd1_Betsafe","Odd_Open_odd1_Betsson","Odd_Open_odd1_Betway","Odd_Open_odd1_Pinnacle","Odd_Open_odd1_SBOBET","Odd_Open_odd1_Unibet","Odd_Open_odd1_WilliamHill","Odd_Open_odd1_bet365","Odd_Open_odd1_betathome","Odd_Open_odd1_bwin","Odd_Open_odd2_10Bet","Odd_Open_odd2_12BET","Odd_Open_odd2_188BET","Odd_Open_odd2_BetVictor","Odd_Open_odd2_Betclic","Odd_Open_odd2_Betsafe","Odd_Open_odd2_Betsson","Odd_Open_odd2_Betway","Odd_Open_odd2_Pinnacle","Odd_Open_odd2_SBOBET","Odd_Open_odd2_Unibet","Odd_Open_odd2_WilliamHill","Odd_Open_odd2_bet365","Odd_Open_odd2_betathome","Odd_Open_odd2_bwin","Odd_Open_oddX_10Bet","Odd_Open_oddX_12BET","Odd_Open_oddX_188BET","Odd_Open_oddX_BetVictor","Odd_Open_oddX_Betclic","Odd_Open_oddX_Betsafe","Odd_Open_oddX_Betsson","Odd_Open_oddX_Betway","Odd_Open_oddX_Pinnacle","Odd_Open_oddX_SBOBET","Odd_Open_oddX_Unibet","Odd_Open_oddX_WilliamHill","Odd_Open_oddX_bet365","Odd_Open_oddX_betathome","Odd_Open_oddX_bwin","Odd_Close_odd1_10Bet","Odd_Close_odd1_12BET","Odd_Close_odd1_188BET","Odd_Close_odd1_BetVictor","Odd_Close_odd1_Betclic","Odd_Close_odd1_Betsafe","Odd_Close_odd1_Betsson","Odd_Close_odd1_Betway","Odd_Close_odd1_Pinnacle","Odd_Close_odd1_SBOBET","Odd_Close_odd1_Unibet","Odd_Close_odd1_WilliamHill","Odd_Close_odd1_bet365","Odd_Close_odd1_betathome","Odd_Close_odd1_bwin","Odd_Close_odd2_10Bet","Odd_Close_odd2_12BET","Odd_Close_odd2_188BET","Odd_Close_odd2_BetVictor","Odd_Close_odd2_Betclic","Odd_Close_odd2_Betsafe","Odd_Close_odd2_Betsson","Odd_Close_odd2_Betway","Odd_Close_odd2_Pinnacle","Odd_Close_odd2_SBOBET","Odd_Close_odd2_Unibet","Odd_Close_odd2_WilliamHill","Odd_Close_odd2_bet365","Odd_Close_odd2_betathome","Odd_Close_odd2_bwin","Odd_Close_oddX_10Bet","Odd_Close_oddX_12BET","Odd_Close_oddX_188BET","Odd_Close_oddX_BetVictor","Odd_Close_oddX_Betclic","Odd_Close_oddX_Betsafe","Odd_Close_oddX_Betsson","Odd_Close_oddX_Betway","Odd_Close_oddX_Pinnacle","Odd_Close_oddX_SBOBET","Odd_Close_oddX_Unibet","Odd_Close_oddX_WilliamHill","Odd_Close_oddX_bet365","Odd_Close_oddX_betathome","Odd_Close_oddX_bwin"))
+  setnames(Data, "DifferenceinGoalDif", "GoalDiff")
+  
   return(as.data.table(Data))
   
 }
 
-
-
+extract_features.pcaComponents <- function(features){
+  
+  data <- features
+  data <- na.omit(data)
+  col <- ncol(data)
+  row <- nrow(data)
+  scaled_data <- data.table()
+  data<- as.data.table(data)
+  scaled_data <- rbind(scaled_data,features[,1:5])
+  colnames <- names(data)
+  
+  for (j in 6:col)
+  {
+    scaled_data[,j] <- scale(data[,get(colnames[j])])
+    j = j+1
+  }
+  setnames(scaled_data, colnames)
+  pca_prin <- princomp(scaled_data[,6:ncol(scaled_data)])
+  load <- pca_prin$loadings
+  load <- load[,1:12]
+  load <- as.matrix(load)
+  
+  s <- scaled_data[,1:5]
+  t <- scaled_data[,6:ncol(scaled_data)]
+  t <- as.matrix(t)
+  
+  r <- t%*%load
+  new_data <- cbind(s,r)
+  
+  loadings = pca_prin$loadings[]
+  p.variance.explained = pca_prin$sdev^2 / sum(pca_prin$sdev^2)
+  
+  # plot percentage of variance explained for each principal component    
+  p.plot <- barplot(100*p.variance.explained[1:12], las=2, xlab='', ylab='% Variance Explained')
+  
+  return(list(p.plot, new_data))
+}
 
 
